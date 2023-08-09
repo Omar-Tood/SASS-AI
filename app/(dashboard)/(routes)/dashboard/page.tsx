3e4ad0f5264ac105a3220a9f994004aa8a0edf8d
Engr-Pirlo <omarjibrilabdulkhadir@gmail.com>
2023-08-10 01:35:35 +0300

    adding new files

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+const   DashboardPage =() => {
+  return (
+        
+    // <Button>Click me</Button>
+    <p>Dashboard Page (Proctected)</p>
+
+  )
+}
+
+export default DashboardPage;
--- app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-const TestPage = ()=>{
-    return(
-        <h1 className="text-pink-600 text-6xl">Welcome to test Page</h1>
-    )
-}
-
-export default TestPage;
